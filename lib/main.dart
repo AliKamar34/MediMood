1138c3cf0a9eaacb5102edf91ee31608c3a6fce6
Ali kamar <aliekamar123@gmail.com>
2024-10-21 22:08:35 +0300

    init hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:new_app/core/utils/app_routes.dart';
 import 'package:new_app/core/utils/colors_asset_data.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
   runApp(const MyApp());
 }
 
