ed19acff0fe00b0a72bd4486c7645f0d0812c0a1
Ali kamar <aliekamar123@gmail.com>
2025-01-25 21:31:13 +0200

    create home repo

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:new_app/core/error/failuer_error.dart';
+import 'package:new_app/features/home/data/models/pill_model.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure, void>> addPill(PillModel pill);
+}
