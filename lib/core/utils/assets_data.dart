b5cfd29287447607fb24387e3b98c6d9179d3f40
Ali kamar <aliekamar123@gmail.com>
2025-01-27 23:49:23 +0200

    add onboarding images and update dependencies for shared_preferences and smooth_page_indicator

--- lib/core/utils/assets_data.dart
+++ lib/core/utils/assets_data.dart
@@ -3,4 +3,7 @@ abstract class AssetsData {
   static const pillIcon = 'assets/images/pill.svg';
   static const noteIcon = 'assets/images/Document.svg';
   static const logo = 'assets/images/logo.png';
+  static const onBoarding1 = 'assets/images/on_boarding_1.png';
+  static const onBoarding2 = 'assets/images/on_boarding_2.png';
+  static const onBoarding3 = 'assets/images/on_boarding_3.png';
 }
